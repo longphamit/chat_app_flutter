82eaf90ed9b82f818bb84b4906afb06c273d1f7e
longphamit <longphamjustdoit@gmail.com>
2021-10-16 18:13:09 +0700

    update

---
+++ lib/model/Message.dart
@@ -0,0 +1,6 @@
+class Message {
+  final String content;
+  final String sender;
+  final String recipient;
+  Message(this.content, this.sender, this.recipient);
+}
---
+++ lib/model/User.dart
@@ -0,0 +1,5 @@
+class User {
+  String name;
+  String id;
+  User(this.name, this.id);
+}
