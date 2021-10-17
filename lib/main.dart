565ea89d5a7bc44132ca196e3c8f3bd07f2012e1
Vinh Nguyen <vinhnquoc196@gmail.com>
2021-10-17 11:07:03 +0700

    update

--- lib/main.dart
+++ lib/main.dart
@@ -15,74 +15,10 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MultiProvider(
       providers: [ChangeNotifierProvider(create: (context) => UserViewModel())],
-      child: MaterialApp(
+      child: const MaterialApp(
         title: "Chat App",
         home: LoginPage(),
       ),
     );
   }
 }
-
-// class MyHomePage extends StatefulWidget {
-//   const MyHomePage({Key? key, required this.title}) : super(key: key);
-
-//   // This widget is the home page of your application. It is stateful, meaning
-//   // that it has a State object (defined below) that contains fields that affect
-//   // how it looks.
-
-//   // This class is the configuration for the state. It holds the values (in this
-//   // case the title) provided by the parent (in this case the App widget) and
-//   // used by the build method of the State. Fields in a Widget subclass are
-//   // always marked "final".
-
-//   final String title;
-
-//   @override
-//   State<MyHomePage> createState() => _MyHomePageState();
-// }
-
-// class _MyHomePageState extends State<MyHomePage> {
-//   @override
-//   void initState() {
-//     super.initState();
-//     ScopedModel.of<ChatModel>(context, rebuildOnChange: false).init();
-//   }
-
-//   void friendClicked(User friend) {
-//     Navigator.of(context).push(
-//       MaterialPageRoute(
-//         builder: (BuildContext context) {
-//           return ChatPage(friend);
-//           //return Container();
-//         },
-//       ),
-//     );
-//   }
-
-//   Widget buildAllChatList() {
-//     return ScopedModelDescendant<ChatModel>(
-//       builder: (context, child, model) {
-//         return ListView.builder(
-//           itemCount: model.friendList.length,
-//           itemBuilder: (BuildContext context, int index) {
-//             User friend = model.friendList[index];
-//             return ListTile(
-//               title: Text(friend.name),
-//               onTap: () => friendClicked(friend),
-//             );
-//           },
-//         );
-//       },
-//     );
-//   }
-
-//   @override
-//   Widget build(BuildContext context) {
-//     return Scaffold(
-//       appBar: AppBar(
-//         title: Text('All Chats'),
-//       ),
-//       body: buildAllChatList(),
-//     );
-//   }
-// }
