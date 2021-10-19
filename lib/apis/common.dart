c3f9527606f34cb417ad659e610a4958b76d8123
Vinh Nguyen <vinhnquoc196@gmail.com>
2021-10-19 14:21:14 +0700

    update

--- lib/apis/common.dart
+++ lib/apis/common.dart
@@ -1,8 +1,6 @@
-const host = "10.0.2.2";
-const port = 5000;
 const hostRemote = "192.168.1.7";
 const portRemote = "3000";
-//const host = "server-chat-demo.herokuapp.com";
+const host = "server-chat-demo.herokuapp.com";
 
 // user api
 const getUserInfoApi = "/user";
